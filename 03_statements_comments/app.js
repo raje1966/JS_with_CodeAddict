d073f7618356598ece12c93850ba1fdcb371c16d
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-18 10:59:22 +0530

    Adding more files to github

---
+++ 03_statements_comments/app.js
@@ -0,0 +1,11 @@
+//I woke, ? up early!!! him today.
+//statements - sets of instructions
+//comments - shortcut Commands +/
+
+//this is console statement
+//console.log('hello world');
+
+/* console.log('hello people');
+console.log('hello my friend bob);
+console.log('hello susan');
+document.write('hello anna'); */
