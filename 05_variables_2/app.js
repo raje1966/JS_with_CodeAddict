008d19597df881932c4b130faf76af53815c6446
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-19 07:07:14 +0530

    adding more files to github

---
+++ 05_variables_2/app.js
@@ -0,0 +1,15 @@
+//VARIABLES --
+// Can contain digits, letters, underscore, $.
+// must start with letter, $ or _
+// no keyword
+// cannot start with numbers
+// case-sensitive - fullname vs fullname
+// camelcase or underscore
+
+let random123_$ = 'random';
+console.log(random123_$);
+
+//better to use camelcase or underscore for variables.
+
+let full_name = 'random';
+console.log(full_name);
