dab0891d375eea6ecc84489199b335bac0f25cdb
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-19 10:17:17 +0530

    Adding more files to github

---
+++ 08_numbers/app.js
@@ -0,0 +1,21 @@
+//Numbers
+//Loosely typed = don't declare type
+
+const number = 34;
+let pants = 2.466;
+// pants = 'are great';
+// console.log(number);
+// console.log(pants);
+const number2 = 2.456;
+const number3 = '2.456';
+console.log(number2);
+console.log(number3);
+const add = number + number2;
+const sub = number - number2;
+const mul = number * number2;
+const div = number / number2;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
