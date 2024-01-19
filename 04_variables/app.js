42ffd915d69ec3cf30cd43acf24ee3940d3773b0
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-19 06:50:59 +0530

    adding more files to github

---
+++ 04_variables/app.js
@@ -0,0 +1,12 @@
+//variables = Most Basic Building Block
+//Variables - store, Access, Modify === value
+//Declare, Assignment Operator, Assign value later, modify existing
+
+let name = "john shrimp taco VI";
+let address, zip, state;
+address = "101 main street";
+zip = "50510";
+state = "CA";
+name = "fish burrito";
+console.log(address, zip, state);
+console.log(name);
