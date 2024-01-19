c56dcf9469dcb164c7a7e4276fe91749aa13e54c
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-19 08:58:58 +0530

    Adding more files to github

---
+++ 07_quotation_marks/app.js
@@ -0,0 +1,16 @@
+// "" or ''
+
+const name = "John's courses are the best";
+// If there is a ' in the sentence, better to use double quotes or `` backticks.
+console.log(name);
+//string concatenation = combining string values.
+// // `` - backticks (template strings) easier option.
+// const name1 = "John";
+// const lastName = "shakeandbake";
+// let fullName = "name1 + ' ' + lastName";
+
+// console.log('hello there your full name is: ' + name1 + ' ' + lastName);
+
+const website = 'google';
+const url = 'https://www.' + website + '.com';
+console.log(url);
