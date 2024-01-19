9d330a83a76c778d3c75c9e87762eaefb994833a
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-19 07:34:00 +0530

    Adding more files to github

---
+++ 06_let_var_const/app.js
@@ -0,0 +1,18 @@
+//let Vs Const Vs Var
+
+//using var - wherein values can be changed.
+
+var value = "some value";
+value = "some other value";
+
+//using let - wherein values can be changed.
+
+let name = "raj";
+name = "bhavana";
+
+//using const (constant) - can't re-assign
+const lastName = "jordan";
+
+console.log(value);
+console.log(name);
+console.log(lastName);
